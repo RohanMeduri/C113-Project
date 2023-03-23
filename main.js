cc3e211265fbad1433b73413a80eaf426bdca51f
RohanMeduri <86210535+RohanMeduri@users.noreply.github.com>
2023-03-22 17:18:11 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preload(){
+}
+function setup(){
+    canvas = createCanvas(700,500);
+    canvas.position(250,350);
+}
+function draw(){
+    circle(30, 30, 40);
+    rect(70, 50, 50, 45);
+    ellipse(306, 346, 55, 55);
+}
